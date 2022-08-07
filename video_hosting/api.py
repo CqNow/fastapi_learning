4fe07a96ac561030c96f57f95bef7ada36eab1e2
cqnow <cqnow1@gmail.com>
2022-08-07 23:19:01 +0300

    add services.py

--- video_hosting/api.py
+++ video_hosting/api.py
@@ -1,22 +1,34 @@
 import shutil
 from typing import List
 
-from fastapi import APIRouter, UploadFile, File, Form
+from uuid import uuid4
+from fastapi import APIRouter, UploadFile, File, Form, HTTPException, BackgroundTasks
+from starlette.responses import StreamingResponse
+# from starlette.templating import Jinja2Templates
 
 from schemas import UploadVideo, GetVideo, Message
 from models import Video, User
+from services import save_video
 
 video_router = APIRouter()
 
 
-@video_router.post('/')
-async def create_video(title: str = Form(...), description: str = Form(...), file: UploadFile = File(...)):
-    info = UploadVideo(title=title, description=description)
-    with open(f'{file.filename}', 'wb') as buffer:
-        shutil.copyfileobj(file.file, buffer)
-    user = await User.objects.first()
-    return await Video.objects.create(file=file.filename, user=user, **info.dict())
+# templates = Jinja2Templates(directory='templates')
 
-@video_router.get('/video/{video_pk}', response_model=GetVideo, responses={404: {'model': Message}})
+
+@video_router.post('/')
+async def create_video(
+        background_tasks: BackgroundTasks,
+        title: str = Form(...),
+        description: str = Form(...),
+        file: UploadFile = File(...),
+):
+    user = await User.objects.first()
+    return await save_video(user, file, title, description, background_tasks)
+
+
+@video_router.get('/video/{video_pk}', responses={404: {'model': Message}})
 async def get_video(video_pk: int):
-    return await Video.objects.select_related('user').get(pk=video_pk)
+    file = await Video.objects.select_related('user').get(pk=video_pk)
+    file_like = open(file.dict().get('file'), mode='rb')
+    return StreamingResponse(file_like, media_type='video/mp4')
