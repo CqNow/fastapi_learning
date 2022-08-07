2724b6e52fa944e029aaa08f669526f3e252bedf
cqnow <cqnow1@gmail.com>
2022-08-07 15:37:09 +0300

    add db connection

--- video_hosting/api.py
+++ video_hosting/api.py
@@ -1,44 +1,22 @@
 import shutil
 from typing import List
-from unittest import result
 
-from fastapi import APIRouter, UploadFile, File, Form, Request
-from fastapi.responses import JSONResponse
-from h11._abnf import status_code
+from fastapi import APIRouter, UploadFile, File, Form
 
-from schemas import UploadVideo, GetVideo, User, Message
+from schemas import UploadVideo, GetVideo, Message
+from models import Video, User
 
 video_router = APIRouter()
 
 
 @video_router.post('/')
-async def root(title: str = Form(...), description: str = Form(...), file: UploadFile = File(...)):
+async def create_video(title: str = Form(...), description: str = Form(...), file: UploadFile = File(...)):
     info = UploadVideo(title=title, description=description)
     with open(f'{file.filename}', 'wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
+    user = await User.objects.first()
+    return await Video.objects.create(file=file.filename, user=user, **info.dict())
 
-    return {'file_name': file.filename, 'info': info}
-
-
-@video_router.post('/img', status_code=666)
-async def upload_image(files: List[UploadFile] = File(...)):
-    for img in files:
-        with open(f'{img.filename}', 'wb') as buffer:
-            shutil.copyfileobj(img.file, buffer)
-
-    return {'result': 'Success!'}
-
-
-@video_router.get('/video', response_model=GetVideo, responses={404: {'model': Message}})
-async def get_video():
-    user = {
-        'id': 25,
-        'name': 'Test_name'
-    }
-    video = {
-        'title': 'Test',
-        'description': 'Description',
-    }
-    info = GetVideo(user=user, video=video)
-    #return info
-    return JSONResponse(status_code=200, content=info.dict())
+@video_router.get('/video/{video_pk}', response_model=GetVideo, responses={404: {'model': Message}})
+async def get_video(video_pk: int):
+    return await Video.objects.select_related('user').get(pk=video_pk)
